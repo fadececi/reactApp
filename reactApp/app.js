72e912d94aef620e61e2f7c13beac1fe49516c02
fadececi <lunazhou96@gmail.com>
2017-06-30 15:31:59 -0700

    got static structure

--- reactApp/app.js
+++ reactApp/app.js
@@ -3,18 +3,69 @@ var ReactDOM = require('react-dom');
 
 
 
-var dummyData=['Do computer science', 'Do science computer', 'Do more'];
+var dummyData=[
+  {taskText:'Do computer science', completed: false},
+  {taskText:'Do science computer', completed: true},
+  {taskText:'Do more', completed: false}  ];
+
+class TodoApp extends React.Component {
+  constructor(props){
+    super(props),
+    this.state={
+      todos:[]
+    }
+  }
+  componentDidMount(){
+    this.setState({todos:dummyData})
+  }
+  render(){
+    return(
+      <div>
+          <InputLine />
+          <TodoList todos={this.state.todos}/>
+                          </div>
+    )
+  }
+}
+
+class InputLine extends React.Component {
+  render(){
+    return(
+      <div>
+        <input type='text' placeholder='task...' />
+        <input type='submit' value="Add" />
+      </div>
+    )
+  }
+}
 
 class TodoList extends React.Component {
   render(){
     return(
       <ul>
-        {this.props.dummyData.map((todo)=>(
-          <li><button className='btn btn-default'>X</button>{'  '}{todo}</li>))}
+        {this.props.todos.map((todo)=>(
+          <Todo todo={todo}/>
+        ))}
       </ul>
     )
   }
 }
 
-ReactDOM.render(<TodoList dummyData={dummyData}/>,
+class Todo extends React.Component {
+  render(){
+    if(this.props.todo.completed){
+      return(<li>
+        <button>X</button>
+        {'  '}<strike>{this.props.todo.taskText}</strike>
+      </li>)
+    } else {
+      return(<li>
+        <button>X</button>
+        {'  '}{this.props.todo.taskText}
+      </li>)
+    }
+  }
+}
+
+ReactDOM.render(<TodoApp />,
    document.getElementById('root'));
